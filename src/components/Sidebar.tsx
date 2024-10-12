8b8d72e70d4ef07a02c71c53a6c43f3c84bbdcd3
sebastiankzk <40329724+sebastiankzk@users.noreply.github.com>
2024-10-13 00:21:07 +0800

    Revert "Merge branch 'master' of https://github.com/seanys-123/Food-Ordering-System into Vendor"
    This reverts commit c6f7502d7a2a19c75970ded29de38b9162e9d3e9, reversing
    changes made to 66f463f7dc0cf87c76ebf6e5766837894c1c8da7.

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,30 +1,23 @@
-// src/components/Sidebar.tsx
 import React from 'react';
 import './Sidebar.css';
 
-interface SidebarProps {
-  handleLogout: () => void; // Prop for the logout function
-}
-
-const Sidebar: React.FC<SidebarProps> = ({ handleLogout }) => {
-  return (
-    <div className="sidebar">
-      <div className="sidebar-logo">Food Hub</div>
-      <ul>
-        <li className="active">Dashboard</li>
+const Sidebar: React.FC = () => {
+    return (
+        <div className="sidebar">
+        <div className="sidebar-logo">Food Hub</div>
+    <ul>
+    <li className="active">Dashboard</li>
         <li>Explore</li>
         <li>Favourite</li>
         <li>Order</li>
         <li>Order History</li>
-        <li>Messages</li>
-        <li>Reviews</li>
-        <li>Settings</li>
-      </ul>
-      <div className="logout" onClick={handleLogout}>
-        Log Out
-      </div>
+    <li>Messages</li>
+    <li>Reviews</li>
+    <li>Settings</li>
+    </ul>
+    <div className="logout">Log Out</div>
     </div>
-  );
+);
 }
 
 export default Sidebar;

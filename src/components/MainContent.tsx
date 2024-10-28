54427a6f6865960a8595fa7a97b22f6416b5c9db
aust0008 <70423409+aust0008@users.noreply.github.com>
2024-10-28 22:16:07 +0800

    added session check for refresh issue

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Categories from './Categories';
 import Menu from './Menu';
 import { MenuItem } from '../type';
